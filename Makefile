2abf5d6852f3f8cc35b5f47c3124b8f36c3d1e7e
Elod Csirmaz <csirmazelodpal@gmail.com>
2024-12-01 16:12:42 +0000

    Set up html reference

--- Makefile
+++
@@ -1,5 +0,0 @@
-
-
-documentation:
-	pdoc --html -f -o doc --config show_inherited_members=True openscad_py
-
--- test.py
+++
@@ -1,7 +0,0 @@
-
-from openscad_py import Cube, Cylinder
-
-
-print(Cube([1,1,1]).render())
-print(Cylinder(h=5, r=2).render())
-print(Cylinder.from_ends(2, [0,0,0], [1,0,0]).render())
